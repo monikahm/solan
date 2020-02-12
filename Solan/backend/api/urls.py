78128672fa755362a2b4ac118d5dad8c348dd1ab
Ole <ole.bjerkeset@hotmail.com>
2020-02-12 20:29:17 +0100

    Closing issue #005

--- Solan/backend/api/urls.py
+++ Solan/backend/api/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path, include
 from rest_framework import routers
 from .views.startups import StartupsView
 
 router = routers.DefaultRouter()
-router.register('api/startups', StartupsView)
+router.register('api/startups', StartupsView, 'startups')
 
 urlpatterns = router.urls
--- Solan/backend/api/views/startups.py
+++ Solan/backend/api/views/startups.py
@@ -1,8 +1,7 @@
 from ..serializers.startups import StartupsSerializer
 from ..models.startups import Startups
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, mixins
 
-class StartupsView(GenericViewSet):
+class StartupsView(GenericViewSet, mixins.RetrieveModelMixin, mixins.ListModelMixin):
     serializer_class = StartupsSerializer
     queryset = Startups.objects.all()
-    search_fields = ('name')
