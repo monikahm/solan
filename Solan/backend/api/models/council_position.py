d7b9e824be0d0f0ddc1ff66be653159bd5094eef
Ole <ole.bjerkeset@hotmail.com>
2020-02-13 18:29:20 +0100

    hotfix

--- Solan/backend/api/models/council_position.py
+++ Solan/backend/api/models/council_position.py
@@ -3,6 +3,6 @@ from .position import *
 
 class CouncilPosition(models.Model):
     name = models.CharField(max_length=50)
-    position = models.ForeignKey(Position, related_name='council_position', blank=True, null=True, on_delete=models.SET(None))
+    position = models.ForeignKey(Position, related_name='council_position', null=True, on_delete=models.SET(None))
     info = models.CharField(max_length=200)
     photo = models.ImageField(upload_to='api/photos',blank=True)
