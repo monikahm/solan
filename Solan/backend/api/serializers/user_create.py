7831fcebdd0a5f590901a22d9430389de838d612
Ole <ole.bjerkeset@hotmail.com>
2020-02-13 16:00:32 +0100

    hotfix

--- Solan/backend/api/serializers/user_create.py
+++ Solan/backend/api/serializers/user_create.py
@@ -9,7 +9,7 @@ class CreateUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('email', 'password', 'first_name','last_name', 'role')
+        fields = ('email', 'password', 'first_name','last_name')
 
     def create(self, validated_data):
         user = super(CreateUserSerializer, self).create(validated_data)
