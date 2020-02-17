c2add651547f6846cce5105dcd7af5e08cacaadc
monikahm <monika.muzyk@hotmail.com>
2020-02-17 15:16:10 +0100

    Fetch startups

---
+++ Solan/frontend/src/components/user.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+function User() {
+    const [startups, setStartup] = useState([]);
+
+    async function fetchStartups() {
+        const res = await fetch("http://127.0.0.1:8000/api/startups/");
+        const data = await res.json()
+        setStartup(data)
+    }
+
+    useEffect(() => {
+        fetchStartups();
+    }, []);
+
+
+    return (
+        <div>
+            {startups.map((s, index) => (
+                <ul key={index}><li key={index}><a href={index}>{s.name}
+                </a></li></ul>
+            ))}
+        </div>
+    )
+}
+
+export default User;
