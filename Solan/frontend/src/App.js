43ee00c68aa8078dadd0239ad0a2e52b5025a3a4
unknown <erling_sm@hotmail.com>
2020-02-17 13:53:09 +0100

    headline

--- Solan/frontend/src/App.js
+++ Solan/frontend/src/App.js
@@ -8,7 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+          <h1>
+            SOLAN Linjeforening
+          </h1>
+
         </p>
         <a
           className="App-link"
