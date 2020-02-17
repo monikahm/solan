8669247eabb21230983fb37dd15d493a3067de71
skymoan <erling_sm@hotmail.com>
2020-02-17 15:50:11 +0100

    added basic profile page component

--- Solan/frontend/src/App.js
+++ Solan/frontend/src/App.js
@@ -9,22 +9,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
 
-          <h1>
-            SOLAN Linjeforening
-          </h1>
+      <userProfile/>;
 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ Solan/frontend/src/components/userProfile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function userProfile() {
+
+    const PHOTO = '...'
+    const USER_NAME = 'Gunnar'
+    const MEMBERSHIP_STATUS = "Member"
+    const EMAIL_ADDRESS = "Mike@solan.com"
+    const WALLET_BALANCE ="100KR"
+
+
+
+}
