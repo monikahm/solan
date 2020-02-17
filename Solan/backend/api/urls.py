4db0e5670c5571f530c3b8b87fd38525bbd0ed20
Ole-Christian Bjerkeset <olechrib@stud.ntnu.no>
2020-02-17 14:48:10 +0100

    New feature: finished issue #008, startups backend Hotfix: changed the name of a couple of the models so it looks better in the admin panel og django

--- Solan/backend/api/urls.py
+++ Solan/backend/api/urls.py
@@ -1,21 +1,23 @@
 from rest_framework import routers
-from .views.startups import StartupsView
+from .views.startup import StartupView
 from django.urls import path, include
 from .views.login import LoginView
 from .views.logout import LogoutView
 from.views.user_create import CreateUserView
 from .views.profile import ProfileView
 from .views.council_position import CouncilPositionView
-from .views.blogposts import BlogPostsView
+from .views.blogpost import BlogPostView
+from .views.partner import PartnerView
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 router = routers.DefaultRouter()
-router.register('api/startups', StartupsView, 'startups')
+router.register('api/startups', StartupView, 'startups')
 router.register('api/profile', ProfileView, 'profile')
 router.register('api/councilposition', CouncilPositionView, 'council_position')
-router.register('api/blogposts', BlogPostsView, 'blogposts')
+router.register('api/blogposts', BlogPostView, 'blogposts')
+router.register('api/partners', PartnerView, 'partners')
 
 urlpatterns = [
     path('', include(router.urls)),
