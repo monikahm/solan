319c92dd0161cb65389ea213fb8a7ebd56522d82
monikahm <monika.muzyk@hotmail.com>
2020-02-18 17:14:59 +0100

    Class to hooks and fix userprofile

--- Solan/frontend/src/App.js
+++ Solan/frontend/src/App.js
@@ -1,21 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Toolbar from "./components/toolbar.js"
 
 
 
-class App extends Component {
-  render() {
-    return (
-        <div className="App">
+function App() {
+  return (
+    <div className="App">
 
-          <Toolbar/>
+      <Toolbar />
 
-
-
-
-        </div>
-    );
-  }
+    </div>
+  );
 }
 export default App;
