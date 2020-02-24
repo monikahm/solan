2fbd6d4e9a284305340bca31ca8d0fb5f7be5bdc
monikahm <monika.muzyk@hotmail.com>
2020-02-24 15:19:22 +0100

    Make api function reusable

--- Solan/frontend/src/components/api.js
+++ Solan/frontend/src/components/api.js
@@ -1,4 +1,21 @@
+import React, { useEffect, useState } from "react";
 
-
-function API() {
+const useFetch = (url, state) => {
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await fetch(url);
+                const json = await result.json();
+                state(json);
+            }
+            catch(error){
+                setError(error);
+            }
+        };
+        fetchData();
+    }, []);
+    console.log({error});
 }
+
+export default useFetch;
