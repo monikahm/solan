6c618d80f6f2f5a36475fb2b92bc4c1857b6449b
skymoan <erling_sm@hotmail.com>
2020-02-26 11:19:20 +0100

    updated styling

--- Solan/frontend/src/App.js
+++ Solan/frontend/src/App.js
@@ -8,23 +8,29 @@ import CouncilList from './components/council/council';
 
 let Home = () => (
   <div>
-    Hello
+
   </div>
 )
 
 const App = () => {
   return (
     <div className="App">
-
-
-
         <nav className="Navbar">
-        <Link to="/">Home</Link> | {" "}
-        <Link to="startups">Startups</Link> | {" "}
-        <Link to="blogs">Blog</Link> | {" "}
-        <Link to="partners">Partners</Link> | {" "}
-        <Link to="council">Council</Link> | {" "}
+            <a href="#default" className="logo">Solan Linjeforening</a>
+
+            <div className="Nav_Links">
+            <Link to="/">Home</Link>
+            <Link to="startups">Startups</Link>
+            <Link to="blogs">Blog</Link>
+            <Link to="council">Council</Link>
+            </div>
       </nav>
+
+
+
+        <nav className="Navbar_footer">
+        <Link to="partners">Partners</Link>
+        </nav>
 
       <Router>
         <Home path="/" />
@@ -33,6 +39,8 @@ const App = () => {
         <Partners path="partners">Partners</Partners>
         <CouncilList path="council">Council</CouncilList>
       </Router>
+
+
     </div>
   );
 }
