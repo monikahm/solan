e2be99cad6c773532efd12aeff46316fc4daa5f9
monikahm <monika.muzyk@hotmail.com>
2020-02-26 16:58:44 +0100

    Add models to init file

--- Solan/backend/api/models/__init__.py
+++ Solan/backend/api/models/__init__.py
@@ -1,1 +1,6 @@
-from .user import User
+from .blogpost import *
+from .council_position import *
+from .info import *
+from .partner import *
+from .startup import *
+from .user import *
--- Solan/backend/api/models/startup.py
+++ Solan/backend/api/models/startup.py
@@ -7,3 +7,4 @@ class Startup(models.Model):
 
     def __str__(self):
         return self.name
+    
