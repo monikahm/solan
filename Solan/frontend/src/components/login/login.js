d8dbcc025ecdf396bafc0bd4a94a00537f4e6ef6
monikahm <monika.muzyk@hotmail.com>
2020-03-15 20:48:31 +0100

    fix linting

--- Solan/frontend/src/components/login/login.js
+++ Solan/frontend/src/components/login/login.js
@@ -1,30 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import './login.css'
-import { LoginAPI, logout } from '../../services/loginservices';
+import { LoginAPI, logout } from '../../services/loginservices'
 
+export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isLogged, setIsLogged] = useState(false)
 
-export default function Login(props) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [isLogged, setIsLogged] = useState(false);
+  function handleSubmit(e) {
+    LoginAPI('http://localhost:8000/api/login', email, password)
+    setIsLogged(true)
+    e.preventDefault()
+  }
 
-    function handleSubmit(e) {
-        LoginAPI('http://localhost:8000/api/login', email, password)
-        setIsLogged(true)
-        e.preventDefault();
-    }
-
-    return (
-        <div className="login">
-            {isLogged ?
-                <button onClick={() => {setIsLogged(false); logout();}}>Logout</button>
-                :
-                <form onSubmit={handleSubmit} >
-                    <input type="email" value={email} name="email" onChange={e => setEmail(e.target.value)}></input>
-                    <input type="password" value={password} name="password" onChange={e => setPassword(e.target.value)}></input>
-                    <input type="submit" onClick={handleSubmit}></input>
-                </form>
-            }
-        </div>
-    )
+  return (
+    <div className="login">
+      {isLogged ? (
+        <button
+          onClick={() => {
+            setIsLogged(false)
+            logout()
+          }}
+        >
+          Logout
+        </button>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            value={email}
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          <input
+            type="password"
+            value={password}
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <input type="submit" onClick={handleSubmit}></input>
+        </form>
+      )}
+    </div>
+  )
 }
--- Solan/frontend/src/services/loginservices.js
+++ Solan/frontend/src/services/loginservices.js
@@ -1,21 +1,20 @@
-
 async function LoginAPI(url, username, password) {
-    const options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            username,
-            password
-        })
-    }
-    return fetch(url, options).then(res => {
-        localStorage.setItem('token', res.token)
-        return Promise.resolve(res)
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      username,
+      password
     })
+  }
+  return fetch(url, options).then((res) => {
+    localStorage.setItem('token', res.token)
+    return Promise.resolve(res)
+  })
 }
 
 function logout() {
-    localStorage.clear();
+  localStorage.clear()
 }
 
 export { LoginAPI, logout }
