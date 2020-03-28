bd644faf1a041360294bbdca91c008c7bbb22d35
monikahm <monika.muzyk@hotmail.com>
2020-03-28 14:59:15 +0100

    Add register page route in login page

--- Solan/frontend/src/components/login/login.js
+++ Solan/frontend/src/components/login/login.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import './login.css'
 import { LoginAPI, logout } from '../../services/loginservices'
+import Register from './register'
+import { Router, Link } from '@reach/router'
 
 export default function Login() {
   const { user, loggedIn } = useSelector((state) => state)
@@ -38,22 +40,30 @@ export default function Login() {
           Logout
         </button>
       ) : (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="email"
-            value={email}
-            name="email"
-            onChange={(e) => setEmail(e.target.value)}
-          ></input>
-          <input
-            type="password"
-            value={password}
-            name="password"
-            onChange={(e) => setPassword(e.target.value)}
-          ></input>
-          <input type="submit" onClick={handleSubmit}></input>
-        </form>
-      )}
+          <>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="email"
+                value={email}
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+              <input
+                type="password"
+                value={password}
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+              <input type="submit" onClick={handleSubmit}></input>
+            </form>
+            <div>
+              <Link to="/register">Registrer her!</Link>
+            </div>
+            <Router>
+              <Register path="/register"></Register>
+            </Router>
+          </>
+        )}
     </div>
   )
 }
