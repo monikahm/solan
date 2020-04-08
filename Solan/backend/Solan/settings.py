b9bb3b64fb0fdc4e77ba14b48115a59ea37d405c
monikahm <monika.muzyk@hotmail.com>
2020-04-08 17:28:25 +0200

    Fix missing css in django

--- Solan/backend/Solan/settings.py
+++ Solan/backend/Solan/settings.py
@@ -27,8 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -125,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'backend')
