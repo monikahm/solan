9e26db1b3ed32ab83cec0f7ae03cfeb4969fb5a3
monikahm <monika.muzyk@hotmail.com>
2020-04-08 20:38:05 +0200

    Change static file path

--- Solan/backend/Solan/settings.py
+++ Solan/backend/Solan/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'backend')
