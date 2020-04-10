c0f449a68b126bc78c55535416fbdc666be5c8c1
tehrafi <mohammrk@stud.ntnu.no>
2020-04-11 01:00:30 +0500

    Add photo from the API.

--- Solan/frontend/src/components/events/eachEventCard.js
+++ Solan/frontend/src/components/events/eachEventCard.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { getDateByString } from '../utils/application.utils';
-const eventIcon = require('./eventIcon.png');
+// const eventIcon = require('./eventIcon.png');
 
 function EventCard(props) {
     const { item } = props;
-    const { title, content, date, signup_link } = item;
+    const { title, content, date, signup_link, photo } = item;
     return (
         <div className="each-event-card">
             <div className="event-upper-body">
-                <img src={eventIcon} onClick={() => {
+                <img src={photo} onClick={() => {
                     window.open(signup_link, "_blank")
                 }} />
                 <p className="event-heading" onClick={() => {
