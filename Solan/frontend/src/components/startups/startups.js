5c186e5d36cb26eb1d919a41194fe778d56221ad
SkyMoan <erlings@stud.ntnu.no>
2020-04-17 10:55:20 +0200

    adjusting startups card according to customer specifications

--- Solan/frontend/src/components/startups/startups.js
+++ Solan/frontend/src/components/startups/startups.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import './startups.css'
 import useFetch from '../api'
-import { truncateTitle } from '../utils/application.utils';
+import { truncateTitle } from '../utils/application.utils'
 
 function Startups() {
-  const [currentWidth, handleWidthChange] = useState(window.innerWidth);
+  const [currentWidth, handleWidthChange] = useState(window.innerWidth)
   const [startups, setStartups] = useState([])
   useFetch('http://127.0.0.1:8000/api/startups/', setStartups)
 
   useEffect(() => {
-    window.addEventListener("resize", getCurrentWidth);
-  });
+    window.addEventListener('resize', getCurrentWidth)
+  })
 
   const getCurrentWidth = () => {
     handleWidthChange(window.innerWidth)
@@ -22,30 +22,21 @@ function Startups() {
       <div className="startups_A">
         <p className="startups_banner_image_text">Startups</p>
       </div>
-      {
-        startups.length !== 0 &&
+      {startups.length !== 0 && (
         <div className="startups_B">
           {startups.map((s, index) => (
             <div className="card" key={index + s}>
-              <div className="card_top"
-              >
+              <div className="card_top">
                 <img className="startupImage" src={s.photo} alt={s.photo} />
               </div>
 
               <div className="card_middle">
                 <div className="startupname">{s.name} </div>
-                <div className="startupinfo" style={{ fontSize: currentWidth > 1200 && '1.7vw' }}>
-                  {truncateTitle(s.info, (currentWidth > 1350 ? 130 : 110))}
-                </div>
-              </div>
-
-              <div className="card_bottom">
-                <button className="readmorebtn">Read More</button>
               </div>
             </div>
           ))}
         </div>
-      }
+      )}
     </div>
   )
 }
