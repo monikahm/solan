a1333399f7e364908254051779db734fdad0b26b
olebje <ole.bjerkeset@hotmail.com>
2020-05-02 19:35:44 +0200

    feature: Write unit-tests for backend

--- Solan/backend/api/models/info.py
+++ Solan/backend/api/models/info.py
@@ -3,3 +3,6 @@ from django.db import models
 class Info(models.Model):
     info = models.TextField(max_length=1000)
     photo = models.ImageField(upload_to='api/photos',blank=True)
+
+    def __str__(self):
+        return self.info
